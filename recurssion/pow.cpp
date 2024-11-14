124d35827f27e5f01d469fb2c38f41b4b4d99d78
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-14 12:05:55 +0530

    pow

--- recurssion/pow.cpp
+++ recurssion/pow.cpp
@@ -1,15 +1,20 @@
 //power using recurssion
 #include <iostream>
 using namespace std;
-int power(int num,int pow) {
-   if(pow==0){
+int power(int num,int pow){
+   if(num==0){
+      return 0;
+   }
+    if(pow==0){
     return 1;
    }
    return num*power(num,pow-1);
 }
-int main() {
+int main(){
 int num,pow;
-cout<<"Enter number and its power ";
-cin>>num>>pow;
+cout<<"Enter number ";
+cin>>num;
+cout<<"Enter power ";
+cin>>pow;
 cout<<power(num,pow);
 }
