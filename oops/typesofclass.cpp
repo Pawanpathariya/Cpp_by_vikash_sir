133ba81346046f70e4eb3efc45f00b28d829c45b
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-21 12:04:41 +0530

    fun overloading static

---
+++ oops/typesofclass.cpp
@@ -0,0 +1,7 @@
+/*types of classes :
+there are many types of classes
+ 1.single class : if a prog having only one class is called single class 
+ 2.anonymous class :
+ 3.multi class(multiple class)
+4.composite class 
+ */
