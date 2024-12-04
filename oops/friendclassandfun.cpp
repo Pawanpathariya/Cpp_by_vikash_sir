b5a40ce3f67316f22ab63a2485b3087a90eb5def
Pawanpathariya <pawanpathariys@gmail.com>
2024-12-04 12:30:36 +0530

    friend class

--- oops/friendclassandfun.cpp
+++ oops/friendclassandfun.cpp
@@ -34,25 +34,22 @@ cmp(obj1,obj2);
 //friend class
 #include<iostream>
 using namespace std;
-class Student2;
-class Student1{
-   int marks=90;
-   friend void cmp(Student1,Student2);
+class Sbi{
+   int amount=9000;
+   void balance(){
+    cout<<"balance "<<amount;
+   }
+   friend class axis;
 };
-class Student2{
-int marks=97;
-   friend void cmp(Student1,Student2);
-};
-void cmp(Student1 s1,Student2 s2){
-  if(s1.marks>s2.marks){
-cout<<"The Student 1 has highest marks "<< s1.marks;
-  }
- else{
-    cout<<"The  Student 2 has highest marks "<< s2.marks;
-  }
+class axis{
+public:
+void display(Sbi sk){
+cout<<" Total balance "<<sk.amount;
 }
+};
+
 int main(){ 
-Student1 obj1;
-Student2 obj2;
-cmp(obj1,obj2);
+Sbi s;
+axis a;
+a.display(s);
 }
