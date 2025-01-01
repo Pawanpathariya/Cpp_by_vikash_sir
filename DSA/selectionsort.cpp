9e058dcc9a3783caf4f2b17d1e92b62cbf7ebbbb
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-01 15:58:20 +0530

    selection sort

---
+++ DSA/selectionsort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void selection(int arr[],int s){
+for(int i=0;i<s;i++){
+    int key=i;
+for(int j=key;j<s;j++){
+    if(arr[key]>arr[j]){
+        key=j;
+    }
+}
+
+if(key!=i){
+    int t=arr[i];
+    arr[i]=arr[key];
+    arr[key]=t;
+}
+}
+}
+
+int main(){
+    int arr[]={3,1,4,5,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Before sorting ";
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    selection(arr,n);
+    cout<<"\nAfter sorting ";
+    selection(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
